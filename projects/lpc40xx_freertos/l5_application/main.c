ed056ecdabfeae79ea9de09f46cff1db19ab0af2
shivanipradeep.tambatkar <shivanipradeep.tambatkar@sjsu.edu>
2020-08-31 20:57:16 -0700

    main.c

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -38,6 +38,7 @@ static void create_blinky_tasks(void) {
 
   xTaskCreate(blink_task, "led0", configMINIMAL_STACK_SIZE, (void *)&led0, PRIORITY_LOW, NULL);
   xTaskCreate(blink_task, "led1", configMINIMAL_STACK_SIZE, (void *)&led1, PRIORITY_LOW, NULL);
+  printf("Hello World!\n");
 #else
   const bool run_1000hz = true;
   const size_t stack_size_bytes = 2048 / sizeof(void *); // RTOS stack size is in terms of 32-bits for ARM M4 32-bit CPU
