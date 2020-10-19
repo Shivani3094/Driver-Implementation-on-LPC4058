93bf8f9020c592eeab04fedf366132f27012f61a
shivanipradeep.tambatkar <shivanipradeep.tambatkar@sjsu.edu>
2020-10-18 17:33:34 -0700

    Completed initial part for Producer and Consumer Lab

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -1,100 +1,101 @@
 #include <stdio.h>
 
-#include "FreeRTOS.h"
-#include "task.h"
+#define PART1
 
+#include "FreeRTOS.h"
 #include "board_io.h"
 #include "common_macros.h"
+#include "delay.h"
 #include "gpio.h"
+#include "lpc_peripherals.h"
 #include "periodic_scheduler.h"
+#include "semphr.h"
 #include "sj2_cli.h"
+#include "task.h"
 
-static void create_blinky_tasks(void);
-static void create_uart_task(void);
-static void blink_task(void *params);
-static void uart_task(void *params);
+void producer(void *p);
+void consumer(void *p);
+
+static QueueHandle_t switch_queue;
+
+typedef enum { switch__off, switch__on } switch_e;
+typedef enum { PRODUCER_HIGH, CONSUMER_HIGH, EQUAL_PRIORITY } set_priority;
+
+switch_e get_switch_input_from_switch0(void);
 
 int main(void) {
-  create_blinky_tasks();
-  create_uart_task();
 
-  puts("Starting RTOS");
-  vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
+#ifdef PART1
+  switch_queue =
+      xQueueCreate(1, sizeof(switch_e)); // Choose depth of item being our enum (1 should be okay for this example)
+
+  set_priority priority = EQUAL_PRIORITY;
+
+  switch (priority) {
+  case 0: {
+    printf("*** PRODUCER HAS HIGHER PRIORITY ***\n\n");
+    xTaskCreate(producer, "tx", 2048 / (sizeof(void *)), NULL, PRIORITY_HIGH, NULL);
+    xTaskCreate(consumer, "rx", 2048 / (sizeof(void *)), NULL, PRIORITY_LOW, NULL);
+    break;
+  }
+
+  case 1: {
+    printf("*** CONSUMER HAS HIGHER PRIORITY ***\n\n");
+    xTaskCreate(producer, "tx", 2048 / (sizeof(void *)), NULL, PRIORITY_LOW, NULL);
+    xTaskCreate(consumer, "rx", 2048 / (sizeof(void *)), NULL, PRIORITY_HIGH, NULL);
+    break;
+  }
+
+  case 2: {
+    printf("*** EQUAL PRIORITY ***\n\n");
+    xTaskCreate(producer, "tx", 2048 / (sizeof(void *)), NULL, PRIORITY_LOW, NULL);
+    xTaskCreate(consumer, "rx", 2048 / (sizeof(void *)), NULL, PRIORITY_LOW, NULL);
+    break;
+  }
+  default:
+    printf("INVALID INPUT");
+  }
+#endif
+
+  vTaskStartScheduler();
 
   return 0;
 }
 
-static void create_blinky_tasks(void) {
-  /**
-   * Use '#if (1)' if you wish to observe how two tasks can blink LEDs
-   * Use '#if (0)' if you wish to use the 'periodic_scheduler.h' that will spawn 4 periodic tasks, one for each LED
-   */
-#if (1)
-  // These variables should not go out of scope because the 'blink_task' will reference this memory
-  static gpio_s led0, led1;
+switch_e get_switch_input_from_switch0(void) {
+  const uint32_t set_switch_as_input = ~(1 << 15);
+  LPC_GPIO1->DIR &= set_switch_as_input;
 
-  led0 = board_io__get_led0();
-  led1 = board_io__get_led1();
+  gpio_s switch0 = {1, 15};
+  switch_e switch_status;
 
-  xTaskCreate(blink_task, "led0", configMINIMAL_STACK_SIZE, (void *)&led0, PRIORITY_LOW, NULL);
-  xTaskCreate(blink_task, "led1", configMINIMAL_STACK_SIZE, (void *)&led1, PRIORITY_LOW, NULL);
-#else
-  const bool run_1000hz = true;
-  const size_t stack_size_bytes = 2048 / sizeof(void *); // RTOS stack size is in terms of 32-bits for ARM M4 32-bit CPU
-  periodic_scheduler__initialize(stack_size_bytes, !run_1000hz); // Assuming we do not need the high rate 1000Hz task
-  UNUSED(blink_task);
-#endif
+  if (gpio__get(switch0)) {
+    switch_status = switch__on;
+  } else {
+    switch_status = switch__off;
+  }
+  return switch_status;
 }
 
-static void create_uart_task(void) {
-  // It is advised to either run the uart_task, or the SJ2 command-line (CLI), but not both
-  // Change '#if (0)' to '#if (1)' and vice versa to try it out
-#if (0)
-  // printf() takes more stack space, size this tasks' stack higher
-  xTaskCreate(uart_task, "uart", (512U * 8) / sizeof(void *), NULL, PRIORITY_LOW, NULL);
-#else
-  sj2_cli__init();
-  UNUSED(uart_task); // uart_task is un-used in if we are doing cli init()
-#endif
-}
+void producer(void *p) {
+  while (1) {
+    const switch_e switch_value = get_switch_input_from_switch0();
 
-static void blink_task(void *params) {
-  const gpio_s led = *((gpio_s *)params); // Parameter was input while calling xTaskCreate()
-
-  // Warning: This task starts with very minimal stack, so do not use printf() API here to avoid stack overflow
-  while (true) {
-    gpio__toggle(led);
-    vTaskDelay(500);
+    printf(" producer() sending message\n");
+    if (!(xQueueSend(switch_queue, &switch_value, 0))) {
+      printf("Failed to send to consumer\n");
+    } else {
+      printf("Successfully sent\n");
+    }
+    vTaskDelay(1000);
   }
 }
 
-// This sends periodic messages over printf() which uses system_calls.c to send them to UART0
-static void uart_task(void *params) {
-  TickType_t previous_tick = 0;
-  TickType_t ticks = 0;
-
-  while (true) {
-    // This loop will repeat at precise task delay, even if the logic below takes variable amount of ticks
-    vTaskDelayUntil(&previous_tick, 2000);
-
-    /* Calls to fprintf(stderr, ...) uses polled UART driver, so this entire output will be fully
-     * sent out before this function returns. See system_calls.c for actual implementation.
-     *
-     * Use this style print for:
-     *  - Interrupts because you cannot use printf() inside an ISR
-     *    This is because regular printf() leads down to xQueueSend() that might block
-     *    but you cannot block inside an ISR hence the system might crash
-     *  - During debugging in case system crashes before all output of printf() is sent
-     */
-    ticks = xTaskGetTickCount();
-    fprintf(stderr, "%u: This is a polled version of printf used for debugging ... finished in", (unsigned)ticks);
-    fprintf(stderr, " %lu ticks\n", (xTaskGetTickCount() - ticks));
-
-    /* This deposits data to an outgoing queue and doesn't block the CPU
-     * Data will be sent later, but this function would return earlier
-     */
-    ticks = xTaskGetTickCount();
-    printf("This is a more efficient printf ... finished in");
-    printf(" %lu ticks\n\n", (xTaskGetTickCount() - ticks));
+void consumer(void *p) {
+  switch_e switch_value;
+  while (1) {
+    printf("consumer() receiving message\n");
+    xQueueReceive(switch_queue, &switch_value, 0);
+    printf("Received from Queue: %d \n\n", switch_value);
   }
 }
